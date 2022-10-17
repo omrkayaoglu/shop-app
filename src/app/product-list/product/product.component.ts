df0a81144014ae62c384865faf40d8ca17250fd4
“sadikturan” <sadikturan@gmail.com>
2022-10-17 14:39:40 +0300

    Ders 20: Product Detay

--- src/app/product-list/product/product.component.ts
+++ src/app/product-list/product/product.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/models/product';
+import { ProductRepository } from 'src/app/models/product.repository';
 
 @Component({
   selector: 'product',
@@ -8,16 +10,18 @@ import { Product } from 'src/app/models/product';
 })
 export class ProductComponent implements OnInit {
 
-  @Input() prd: Product
-  @Output() unSelectEvent = new EventEmitter<void>();
+  product: Product | undefined;
+  productRepository: ProductRepository;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private route: ActivatedRoute) {
+    this.productRepository = new ProductRepository();
   }
 
-  unselectProduct() {
-    this.unSelectEvent.emit();
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      const id = params["productId"];
+      this.product = this.productRepository.getProductById(id);
+    });
   }
 
 }
