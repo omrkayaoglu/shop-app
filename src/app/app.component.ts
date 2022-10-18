67516b0f576d056dddaf1980cc96ed66dc12372f
“sadikturan” <sadikturan@gmail.com>
2022-10-18 14:21:17 +0300

    Ders 24: Firebase ile Veri Kayıt Etme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
-  // selector: 'app',
-  // selector: '.app',
   selector: '#app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
@@ -10,7 +9,26 @@ import { Component } from '@angular/core';
 export class AppComponent {
   private title = 'Home Page';
 
+  constructor(private http: HttpClient) { }
+
   getTitle() {
     return this.title;
+  }
+
+  createProduct() {
+
+    const product = { 
+      id: 1, 
+      name: "iphone 22", 
+      price: 20000, 
+      imageUrl: "3.jpeg", 
+      description: "iyi telefon", 
+      isActive: true, 
+      categoryId: 1 
+    }
+
+    this.http.post('<api_adresiniz>', product)
+      .subscribe(data => console.log(data));
+
   }
 }
