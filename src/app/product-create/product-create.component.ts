0daf82cbb0aa0e42a6fb7cd20a193a7ecf8f53cd
“sadikturan” <sadikturan@gmail.com>
2022-10-21 15:27:08 +0300

    Ders 30: Category Service

--- src/app/product-create/product-create.component.ts
+++ src/app/product-create/product-create.component.ts
@@ -1,20 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Category } from '../models/category';
+import { CategoryService } from '../services/category.service';
 import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-product-create',
   templateUrl: './product-create.component.html',
-  styleUrls: ['./product-create.component.css']
+  styleUrls: ['./product-create.component.css'],
+  providers: [CategoryService]
 })
 export class ProductCreateComponent implements OnInit {
 
+  categories: Category[] = [];
+
   constructor(
     private productService: ProductService,
+    private categoryService: CategoryService,
     private router: Router
     ) { }
 
   ngOnInit(): void {
+    this.categoryService.getCategories().subscribe(data => {
+      this.categories = data;
+    });
   }
 
   saveProduct(name:any,price:any,imageUrl:any,description:any,isActive:any,categoryId:any) {
