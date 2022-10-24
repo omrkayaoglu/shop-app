208c83c28680f5a757a643d5e98d8109bc8cc691
“sadikturan” <sadikturan@gmail.com>
2022-10-24 16:34:31 +0300

    Ders 33: Template Driven-Forms

--- src/app/product-create/product-create.component.ts
+++ src/app/product-create/product-create.component.ts
@@ -14,6 +14,13 @@ export class ProductCreateComponent implements OnInit {
 
   categories: Category[] = [];
   error: string = "";
+  model: any = {
+    name: "iphone 17",
+    price: 20000,
+    categoryId: "0"
+  };
+  // two-way binding
+  // ngModel => control
 
   constructor(
     private productService: ProductService,
@@ -27,49 +34,54 @@ export class ProductCreateComponent implements OnInit {
     });
   }
 
-  saveProduct(name:any,price:any,imageUrl:any,description:any,isActive:any,categoryId:any) {
+  saveProduct() {
 
-    if(name.value == "" || name.value.length < 5) {
-      this.error = "ürün ismi en az 5 karakter giriniz.";
-      return;
-    }
+    // name:any,price:any,imageUrl:any,description:any,isActive:any,categoryId:any
+    // if(name.value == "" || name.value.length < 5) {
+    //   this.error = "ürün ismi en az 5 karakter giriniz.";
+    //   return;
+    // }
 
-    if(price.value == "") {
-      this.error ="ürün firyatı girmelisiniz.";
-      return;
-    }
+    // if(price.value == "") {
+    //   this.error ="ürün firyatı girmelisiniz.";
+    //   return;
+    // }
 
-    if(imageUrl.value == "") {
-      this.error ="resim ismi girmelisiniz.";
-      return;
-    }
+    // if(imageUrl.value == "") {
+    //   this.error ="resim ismi girmelisiniz.";
+    //   return;
+    // }
 
-    const extensions = ["jpeg","jpg","png"];
-    const extension = imageUrl.value.split(".").pop();
+    // const extensions = ["jpeg","jpg","png"];
+    // const extension = imageUrl.value.split(".").pop();
 
-    if(extensions.indexOf(extension) == -1) {
-      this.error ="resim uzantısı sadece jpeg, jpg, png olmalıdır.";
-      return;
-    }
+    // if(extensions.indexOf(extension) == -1) {
+    //   this.error ="resim uzantısı sadece jpeg, jpg, png olmalıdır.";
+    //   return;
+    // }
 
-    if(categoryId.value == "0") {
-      this.error ="kategori seçmelisiniz.";
-      return;
-    }
+    // if(categoryId.value == "0") {
+    //   this.error ="kategori seçmelisiniz.";
+    //   return;
+    // }
 
-    const product = { 
-      id: 1, 
-      name: name.value, 
-      price: price.value, 
-      imageUrl: imageUrl.value, 
-      description: description.value, 
-      isActive: isActive.checked, 
-      categoryId: categoryId.value
-    }
+    // const product = { 
+    //   id: 1, 
+    //   name: name.value, 
+    //   price: price.value, 
+    //   imageUrl: imageUrl.value, 
+    //   description: description.value, 
+    //   isActive: isActive.checked, 
+    //   categoryId: categoryId.value
+    // }
 
-    this.productService.createProduct(product).subscribe(data => {
-      this.router.navigate(['/products']);
-    });
+    // this.productService.createProduct(product).subscribe(data => {
+    //   this.router.navigate(['/products']);
+    // });
+
+    console.log(this.model);
+
+
 
   }
 
